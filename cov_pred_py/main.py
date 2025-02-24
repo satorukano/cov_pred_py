b0aa28633c7392920c7515a08ede3bc2af8f394c
satorukano <kiritomo0206@icloud.com>
2025-02-25 02:04:57 +0900

    add pickle

--- cov_pred_py/main.py
+++ cov_pred_py/main.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 
 from module.identify_execute_block import IdentifyExecuteBlock
 from db.database import Database
@@ -8,7 +9,7 @@ def main():
     signatures = db.get_signatures(1)
     json_open = open("block_range.json", 'r')
     block_range = json.load(json_open)
-    ans_signature = signatures[1][0]
+    ans_signature = signatures[0][0]
     ans_ieb = IdentifyExecuteBlock(block_range, db, ans_signature)
     ans_labels = {}
     ans_vector = ans_ieb.create_vector()
@@ -19,6 +20,7 @@ def main():
             continue
         ieb = IdentifyExecuteBlock(block_range, db, signature[0])
         ans_labels[signature] = ieb.create_vector_with_executed_block(executed_block)
-    print(ans_labels)
+    path_file = 'labels.pkl'
+    pickle.dump(ans_labels, open(path_file, 'wb'))
 
 main()
